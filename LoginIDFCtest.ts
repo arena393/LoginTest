cae526791c1b44a9f8207a074dc1a44ca3b6d7d9
arena393 <165892419+arena393@users.noreply.github.com>
2024-05-09 16:17:51 +0530

    Add files via upload

---
+++ LoginIDFCtest.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>To test in Katalon Testops</description>
+   <name>LoginIDFCtest</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4e723292-826f-40b9-b66a-0d3b63425eeb</testSuiteGuid>
+   <testCaseLink>
+      <guid>c13335af-e8d7-44a5-ba66-a761dd239a44</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_IDFC_TEST</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
